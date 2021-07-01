917e11533abc898bb5a3adf8931092f987a39a2c
Bruno <brunotardioti@gmail.com>
2021-07-01 10:45:48 -0300

    first commit

---
+++ RestWithASP-NET5.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestWithASP_NET5._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
